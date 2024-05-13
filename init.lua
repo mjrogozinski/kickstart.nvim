a9da8b794d9bcb0f7a4a43c1eaedbc213e07985e
mjrogozinski <mjrogozinski@gmail.com>
2024-05-13 15:29:54 +0200

    <leader>h for noh, like in lunar vim

--- init.lua
+++ init.lua
@@ -383,7 +383,8 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
-      vim.keymap.set('n', '<leader>C', ':e $MYVIMRC<CR>', { desc = 'edit [C]onfiguration' })
+      vim.keymap.set('n', '<leader>C', ':e $MYVIMRC<CR>', { desc = '[C]onfiguration Edit' })
+      vim.keymap.set('n', '<leader>h', ':noh<CR>', { desc = '[N][o] [h]ighlight' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>?', function()
