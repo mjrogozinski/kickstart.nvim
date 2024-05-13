86afdd56ab51b77fa2b3d2f1d8a80025e27b065e
mjrogozinski <mjrogozinski@gmail.com>
2024-05-13 18:30:15 +0200

    groundwork for C++ support

--- init.lua
+++ init.lua
@@ -106,6 +106,7 @@ vim.opt.number = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
+vim.opt.autochdir = true
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
@@ -568,7 +569,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
