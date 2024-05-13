8942973947d5f77a53bc5b05b9ff9eed88a0a1be
mjrogozinski <mjrogozinski@gmail.com>
2024-05-13 14:25:31 +0200

    adding file explorer <leader>e

--- init.lua
+++ init.lua
@@ -888,6 +888,23 @@ require('lazy').setup({
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   -- { import = 'custom.plugins' },
+  {
+    'echasnovski/mini.files',
+    version = '*',
+    keys = {
+      {
+        '<leader>e',
+        function()
+          MiniFiles.open()
+        end,
+        mode = '',
+        desc = '[e]xplorer',
+      },
+    },
+    config = function()
+      require('mini.files').setup()
+    end,
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
