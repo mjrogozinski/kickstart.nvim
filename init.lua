2dcecd4d8119daf96f266f430b6eb35073aa112e
mjrogozinski <mjrogozinski@gmail.com>
2024-06-29 18:09:44 +0200

    Alt+j Alt+k to move line up or down

--- init.lua
+++ init.lua
@@ -196,6 +196,8 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<M-j>', ':m +1<CR>', { desc = 'Move line down' })
+vim.keymap.set('n', '<M-k>', ':m -2<CR>', { desc = 'Move line up' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
