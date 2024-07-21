aaf8512c84de5a7fd30e0e2574934950f4ebce3a
mjrogozinski <mjrogozinski@gmail.com>
2024-07-21 15:34:59 +0200

    Lspsaga keybindings

--- lua/custom/plugins/lsp-saga-config.lua
+++ lua/custom/plugins/lsp-saga-config.lua
@@ -6,7 +6,25 @@ return {
       '<leader>cA',
       '<cmd>Lspsaga code_action<CR>',
       mode = '',
-      desc = 'lspsaga code action',
+      desc = 'Lspsaga Code Action',
+    },
+    {
+      '<leader>cA',
+      '<cmd>Lspsaga range_code_action<CR>',
+      mode = 'x',
+      desc = 'Range code action',
+    },
+    {
+      '<leader>cd',
+      '<cmd>Lspsaga show_line_diagnostics<CR>',
+      mode = '',
+      desc = 'Show line diagnostics',
+    },
+    {
+      '<leader>cR',
+      '<cmd>Lspsaga rename<CR>',
+      mode = '',
+      desc = 'Lspsaga rename',
     },
   },
   dependencies = {
