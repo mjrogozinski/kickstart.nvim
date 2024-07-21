68902a4e5b1bc61d133246c107077cf04429901a
mjrogozinski <mjrogozinski@gmail.com>
2024-07-21 14:04:42 +0200

    lsp saga keymap

--- lua/custom/plugins/lsp-saga-config.lua
+++ lua/custom/plugins/lsp-saga-config.lua
@@ -1,6 +1,14 @@
 return {
   'nvimdev/lspsaga.nvim',
   opts = {},
+  keys = {
+    {
+      '<leader>cA',
+      '<cmd>Lspsaga code_action<CR>',
+      mode = '',
+      desc = 'lspsaga code action',
+    },
+  },
   dependencies = {
     'nvim-treesitter/nvim-treesitter', -- optional
     'nvim-tree/nvim-web-devicons', -- optional
