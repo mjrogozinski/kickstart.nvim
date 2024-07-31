9ca97f0ad751a8442722810b3f8aea1d3250335d
Michal Rogozinski <michal.rogozinski@nokia.com>
2024-07-31 18:56:57 +0300

    clangd toggle inlay hints

--- lua/custom/plugins/clang-tools-extra-config.lua
+++ lua/custom/plugins/clang-tools-extra-config.lua
@@ -1,4 +1,7 @@
 return {
   'p00f/clangd_extensions.nvim',
   opts = {},
+  config = function()
+    vim.keymap.set('n', '<leader>th', ':ClangdToggleInlayHints', { desc = 'Toggle inlay hints (clangd)' })
+  end,
 }
