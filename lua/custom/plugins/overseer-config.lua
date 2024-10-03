c2257f7d425a60b2c53bce929e775ef26c9128df
mjrogozinski <mjrogozinski@gmail.com>
2024-10-03 22:54:37 +0200

    reformatting overseer config

--- lua/custom/plugins/overseer-config.lua
+++ lua/custom/plugins/overseer-config.lua
@@ -1,1 +1,11 @@
-return { 'stevearc/overseer.nvim', opts = { templates = { 'builtin', 'user.run_script', 'user.compile_cpp', 'user.build_cmake' } } }
+return {
+  'stevearc/overseer.nvim',
+  opts = {
+    templates = {
+      'builtin',
+      'user.run_script',
+      'user.compile_cpp',
+      'user.build_cmake',
+    },
+  },
+}
