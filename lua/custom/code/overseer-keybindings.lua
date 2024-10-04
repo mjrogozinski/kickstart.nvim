9fe184109a4f731f1b159641094240fd2f7acec9
mjrogozinski <mjrogozinski@gmail.com>
2024-10-04 19:53:21 +0200

    Found a better way to run overseer tasks!

--- lua/custom/code/overseer-keybindings.lua
+++ lua/custom/code/overseer-keybindings.lua
@@ -1,5 +1,4 @@
--- at the moment I don't know how to do it better
--- there should be a way to add bindings using overseer's API
+-- another option is :OverseerRun cmake\ build
 vim.keymap.set('n', '<leader>cb', function()
   require('overseer').run_template { name = 'cmake build' }
 end, {
