0e4571bc1378904b6b5f657d9e61877d7ae95464
mjrogozinski <mjrogozinski@gmail.com>
2024-10-13 17:51:17 +0200

    Treesitter text objects

--- init.lua
+++ init.lua
@@ -774,6 +774,7 @@ require('lazy').setup({
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-cmdline',
     },
     config = function()
       -- See `:help cmp`
@@ -887,7 +888,7 @@ require('lazy').setup({
       --  - va)  - [V]isually select [A]round [)]paren
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
       --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
+      -- require('mini.ai').setup { n_lines = 500 }
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
@@ -1000,6 +1001,7 @@ require 'custom.code.execute-lua-file'
 require 'custom.code.neotest-start-config'
 require 'custom.code.overseer-keybindings'
 require 'custom.code.close-qflist'
+require 'custom.code.ts-textobjects'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/custom/plugins/ts-textobjects.lua
+++ lua/custom/plugins/ts-textobjects.lua
@@ -1,1 +1,4 @@
-return { 'nvim-treesitter/nvim-treesitter-textobjects' }
+return {
+  'nvim-treesitter/nvim-treesitter-textobjects',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+}
