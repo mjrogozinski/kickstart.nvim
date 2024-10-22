e0775c82db81684c4da1ef51b71be8241ee02189
mjrogozinski <mjrogozinski@gmail.com>
2024-10-22 20:38:14 +0200

    Starting lua debug server with leader dl
    It's only available in lua buffers to save keymaps

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -2,3 +2,7 @@ local in_current_buffer = require 'custom.code.utils.buf-key-map'
 
 vim.keymap.set('n', '<leader>x', ':source %<cr>', in_current_buffer.with_desc 'execute lua file')
 vim.keymap.set('n', '<leader>X', ':lua require"osv".run_this()<cr>', in_current_buffer.with_desc 'debug lua file (breakpoint required!)')
+
+vim.keymap.set('n', '<leader>dl', function()
+  require('osv').launch { port = 8086 }
+end, in_current_buffer.with_desc ' Launch Lua debug adapter')
