b0ddb443052167db118e9a9b012ef9c00d73f59c
mjrogozinski <mjrogozinski@gmail.com>
2024-11-03 14:03:49 +0100

    switching to the last split did not work when more splits were open

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -2,18 +2,10 @@ local in_current_buffer = require 'custom.code.utils.buf-key-map'
 
 vim.keymap.set('n', '<leader>ct', function()
   vim.cmd.RustLsp { 'testables', bang = false }
-  vim.cmd 'wincmd w'
 end, in_current_buffer.with_desc 'RustTest!')
-
-local function switch_to_previous_split()
-  vim.defer_fn(function()
-    vim.cmd 'wincmd w'
-  end, 100)
-end
 
 vim.keymap.set('n', '<leader>x', function()
   vim.cmd.RustLsp 'run'
-  switch_to_previous_split()
 end, in_current_buffer.with_desc 'Rust run')
 
 vim.keymap.set('n', '<leader>X', function()
